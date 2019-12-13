51defebd138ca92cec666d1c1e4ab309be65f1bd
Lizzie <lchan217@gmail.com>
2019-12-13 10:21:58 -0700

    add checkbox

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ $(function addPodcast() {
   $(".add-podcast").on("click", function() {
     event.preventDefault();
     var podcastItem = $(".podcast-input").val();
-    var podcastItemHtml = "<li>" + podcastItem + "</li>";
+    var podcastItemHtml =
+      "<div><span class='podcast-checkbox'></span><span class='podcast-text'>" +
+      podcastItem +
+      "</span><span class='podcast-remove'></div>";
 
     $(".podcast-list").prepend(podcastItemHtml);
     $(".podcast-input").val("");
