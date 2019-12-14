a11f6e0c32b2e0eb5843400004c06f1cf4add5bb
Lizzie <lchan217@gmail.com>
2019-12-14 11:10:39 -0700

    add css from lecture

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 $(function addPodcast() {
   $(".add-podcast").on("click", function() {
     event.preventDefault();
-    var podcastItem = $(".podcast-input").val();
-    var podcastItemHtml =
-      "<li><span class='podcast-checkbox'></span><span class='podcast-text'>" +
-      podcastItem +
+    var podcastpodcast = $(".podcast-input").val();
+    var podcastpodcastHtml =
+      "<li><span class='podcast-check'></span><span class='podcast-text'>" +
+      podcastpodcast +
       "</span><span class='podcast-remove'></li>";
 
-    $(".podcast-list").prepend(podcastItemHtml);
+    $(".podcast-list").prepend(podcastpodcastHtml);
     $(".podcast-input").val("");
   });
 });
 
-$(function removeItem() {
+$(function removepodcast() {
   $(".podcast-list").on("click", ".podcast-remove", function(event) {
     $(event.currentTarget)
       .parent()
@@ -21,7 +21,11 @@ $(function removeItem() {
 });
 
 $(function markComplete() {
-  $(".podcast-list").on("click", ".podcast-checkbox", function(event) {
+  $(".podcast-list").on("click", ".podcast-check", function(event) {
     $(event.currentTarget).toggleClass("complete");
   });
 });
+
+//Additional Challenges
+//If you add an podcast with no text, it adds a blank box. This is a bug! Fix it.
+//Add a counter that tells the user how many podcasts are in your podcast list
